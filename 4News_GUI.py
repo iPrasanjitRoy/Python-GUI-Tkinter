de422fa59152abb89c785a633b786f666c4596b1
iPrasanjitRoy <EmailPrasanjitRoy@gmail.comm>
2023-12-28 20:05:50 +0530

    Tkinter

---
+++ 4News_GUI.py
@@ -0,0 +1,107 @@
+import io
+import webbrowser
+import requests
+from tkinter import *
+from urllib.request import urlopen
+from PIL import ImageTk, Image
+
+
+class NewsApp:
+    def __init__(self):
+        self.data = requests.get(
+            "https://newsapi.org/v2/top-headlines?country=in&apiKey=d05563598bc243bcb34dcc5073b8c59f"
+        ).json()
+        self.load_gui()
+        self.load_news_item(0)
+
+    def load_gui(self):
+        self.root = Tk()
+        self.root.geometry("350x600")
+        self.root.resizable(0, 0)
+        self.root.title("My News App")
+        self.root.configure(background="black")
+
+    # Clear Any Existing Content In The Tkinter Window
+    def clear(self):
+        for i in self.root.pack_slaves():
+            i.destroy()
+
+    def load_news_item(self, index):
+        self.clear()
+
+        try:
+            img_url = self.data["articles"][index]["urlToImage"]
+            raw_data = urlopen(img_url).read()
+            img = Image.open(io.BytesIO(raw_data)).resize((350, 250))
+            photo = ImageTk.PhotoImage(img)
+        except:
+            img_url = "https://upload.wikimedia.org/wikipedia/commons/7/75/No_image_available.png"
+            raw_data = urlopen(img_url).read()
+            img = Image.open(io.BytesIO(raw_data)).resize((350, 250))
+            photo = ImageTk.PhotoImage(img)
+
+        label = Label(self.root, image=photo)
+        label.pack()
+
+        heading = Label(
+            self.root,
+            text=self.data["articles"][index]["title"],
+            bg="black",
+            fg="white",
+            wraplength=350,
+            justify="center",
+        )
+        heading.pack(pady=(10, 20))
+        heading.config(font=("verdana", 15))
+
+        details = Label(
+            self.root,
+            text=self.data["articles"][index]["description"],
+            bg="black",
+            fg="white",
+            wraplength=350,
+            justify="center",
+        )
+        details.pack(pady=(2, 20))
+        details.config(font=("verdana", 12))
+
+        # Create Buttons For Navigation
+        frame = Frame(self.root, bg="black")
+        frame.pack(expand=True, fill=BOTH)
+
+        if index != 0:
+            prev = Button(
+                frame,
+                text="Prev",
+                width=16,
+                height=3,
+                command=lambda: self.load_news_item(index - 1),
+            )
+            prev.pack(side=LEFT)
+
+        read = Button(
+            frame,
+            text="Read More",
+            width=16,
+            height=3,
+            command=lambda: self.open_link(self.data["articles"][index]["url"]),
+        )
+        read.pack(side=LEFT)
+
+        if index != len(self.data["articles"]) - 1:
+            next = Button(
+                frame,
+                text="Next",
+                width=16,
+                height=3,
+                command=lambda: self.load_news_item(index + 1),
+            )
+            next.pack(side=LEFT)
+
+        self.root.mainloop()
+
+    def open_link(self, url):
+        webbrowser.open(url)
+
+
+obj = NewsApp()
